c36be4edfd361ea4bafd67c8f0a72e68326dfa14
Behzad <behzad.piri70@gmail.com>
2025-04-14 14:15:52 +0330

    create container component

---
+++ public/components/index.tsx
@@ -0,0 +1,7 @@
+export {default as Container} from "./container/container";
+export {default as Navbar} from "./navbar/navbar";
+
+
+
+/*Cards*/
+export {default as ProductCard} from "./cards/productCard/productCard"
